a01e21d503ac598b61e5de8a7894e4b32109bf81
IoannisIatrakis <cs05116@uoi.gr>
2025-11-03 20:59:54 +0200

    Update insertData.sql path

--- Database/DDL script/insertData.sql
+++ Database/DDL script/insertData.sql
@@ -1,17 +1,17 @@
 set global local_infile = 1;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\giannis\\Documents\\8osemester\\pvasil\\dataETL\\FinaldataETL\\countriesETLShorter.csv' INTO TABLE Countries 
+LOAD DATA LOCAL INFILE 'Data\\FinaldataETL\\countriesETLShorter.csv' INTO TABLE Countries 
 FIELDS TERMINATED BY ';'  LINES TERMINATED BY '\n' IGNORE 1 LINES
 (ISO3, CountryName, Capital, Continent, CurrencyCode, RegionCode, RegionName, SubRegionCode, SubRegionName, CountryStatus, Area_SqKm, CountryPopulation);
 
-LOAD DATA LOCAL INFILE "C:\\Users\\giannis\\Documents\\8osemester\\pvasil\\dataETL\\FinaldataETL\\DisastersETL.csv" INTO TABLE Disasters FIELDS
+LOAD DATA LOCAL INFILE "Data\\FinaldataETL\\DisastersETL.csv" INTO TABLE Disasters FIELDS
 TERMINATED BY ";" ENCLOSED BY '\"' LINES TERMINATED BY "\n" IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE "C:\\Users\\giannis\\Documents\\8osemester\\pvasil\\dataETL\\FinaldataETL\\ForestCarbonETL.csv" INTO TABLE ForestCarbon FIELDS
+LOAD DATA LOCAL INFILE "Data\\FinaldataETL\\ForestCarbonETL.csv" INTO TABLE ForestCarbon FIELDS
 TERMINATED BY ";" ENCLOSED BY '\"' LINES TERMINATED BY "\n" IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE "C:\\Users\\giannis\\Documents\\8osemester\\pvasil\\dataETL\\FinaldataETL\\LandETL.csv" INTO TABLE Land FIELDS
+LOAD DATA LOCAL INFILE "Data\\FinaldataETL\\LandETL.csv" INTO TABLE Land FIELDS
 TERMINATED BY ";" ENCLOSED BY '\"' LINES TERMINATED BY "\n" IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE "C:\\Users\\giannis\\Documents\\8osemester\\pvasil\\dataETL\\FinaldataETL\\TemperatureETL.csv" INTO TABLE Temperature FIELDS
+LOAD DATA LOCAL INFILE "Data\\FinaldataETL\\TemperatureETL.csv" INTO TABLE Temperature FIELDS
 TERMINATED BY ";" ENCLOSED BY '\"' LINES TERMINATED BY "\n" IGNORE 1 LINES;
